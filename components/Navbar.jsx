83e2cd260227f4962ec7fdf05ef9897dfd55ff7b
houariDz95 <houarieddrief7@gmail.com>
2022-10-01 17:46:43 +0100

    fix bag

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
                   </div>
                 </div>
                 <div className="border-b border-gray-300 my-4">
-                  <p className="w-[85%] md:w-[90%] py-4">Let's build something legendary togather</p>
+                  <p className="w-[85%] md:w-[90%] py-4">Lets build something legendary togather</p>
                 </div>
               </div>
               <div className="py-4 flex flex-col">
@@ -67,7 +67,7 @@ const Navbar = () => {
                   </Link>
                 </ul>
                 <div className="pt-4">
-                  <p className="uppercase tracking-widest text-[#5651e5]">Let's Connect</p>
+                  <p className="uppercase tracking-widest text-[#5651e5]">Lets Connect</p>
                   <div> 
                     <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
                       <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105
