ae76ceb91a892c74cce5fa4b4aeccdd9c73f19c0
houariDz95 <houarieddrief7@gmail.com>
2022-10-01 17:37:07 +0100

    added resume

---
+++ pages/resume.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import Head from 'next/head';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+const resume = () => {
+  return (
+    <>
+      <Head>
+        <title>Eddrief | Resume</title>
+        <meta
+          name='description'
+          content='I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences.'
+        />
+        <link rel='icon' href='/fav.png' />
+      </Head>
+
+      <div className='max-w-[940px] mx-auto p-2 pt-[120px]'>
+        <h2 className='text-center'>Resume</h2>
+        <div className='bg-[#d0d4d6] my-4 p-4 w-full flex justify-between items-center'>
+          <h2 className='text-center'>Eddrief Houari</h2>
+          <div className='flex'>
+            <a
+              href='https://www.linkedin.com/in/houari-eddrief-198858240/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FaLinkedinIn size={20} style={{ marginRight: '1rem' }} />
+            </a>
+            <a
+              href='https://github.com/houariDz95'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FaGithub size={20} style={{ marginRight: '1rem' }} />
+            </a>
+          </div>
+        </div>
+        <div className='text-center py-4 text-xl font-bold uppercase tracking-wider'>
+          <div className='hidden sm:block'>
+            <p>
+              safety engineer <span className='px-1'>|</span> Web Development{' '}
+              <span className='px-1'>|</span> Complex Problem Solving
+            </p>
+          </div>
+          <div className='block sm:hidden'>
+            <p>Proven Leadership</p>
+            <p className='py-2'>Web Development</p>
+            <p>Complex Problem Solving</p>
+          </div>
+        </div>
+        <p>
+          Analytical, innovative, and motivated web development professional
+          with experience in customer service, team leadership, and
+          organizational effectiveness in fast-paced and challenging
+          environments. Adept at developing strategies and driving streamlined
+          operations. Diverse analytical skills, team collaboration, and
+          relationship building. Consummate professional, and motivated leader,
+          with solid interpersonal abilities and complex problem-solving skills.
+          Effective and proven track record of critical thinking, idea
+          generation, and optimizing efficiencies.
+        </p>
+
+        {/* Skills */}
+        <div className='text-center py-4'>
+          <h5 className='text-center underline text-[18px] py-2'>Skills</h5>
+          <p className='py-2'>
+            <span className='font-bold'>Technical Skills</span>
+            <span className='px-2'>|</span>Front-End Web Developer
+            <span className='px-2'>|</span> HTML
+            <span className='px-2'>|</span>CSS
+            <span className='px-2'>|</span>Javascript
+            <span className='px-2'>|</span>React
+            <span className='px-2'>|</span>Next JS
+            <span className='px-2'>|</span>SQL
+            <span className='px-2'>|</span>NoSQL
+            <span className='px-2'>|</span>Redux
+            <span className='px-2'>|</span>Tailwind
+            <span className='px-2'>|</span> Firebase
+            <span className='px-2'>|</span> RESTAPI
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default resume;
